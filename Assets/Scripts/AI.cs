718bacc92727ac64528bf7e86bad10c73783bb4e
pijussJ <pijus.jarusevicius3@gmail.com>
2024-01-07 15:46:24 +0200

    Visuals

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public static string[] type = new string[] { "C", "H", "D", "S" };
+    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    public List<string> deck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
