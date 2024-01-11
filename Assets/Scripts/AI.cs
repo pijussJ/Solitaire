2f4956aeefa4c26b405e9dba4ee1a14206441a74
pijussJ <pijus.jarusevicius3@gmail.com>
2024-01-11 17:13:16 +0200

    Dealinimo pradzia

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -4,19 +4,67 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
+    public Sprite[] cardFaces;
+    public GameObject cardPrefab;
     public static string[] type = new string[] { "C", "H", "D", "S" };
     public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
     public List<string> deck;
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        deck = GenerateDeck();
+        Shuffle(deck);
+
+        // test
+        foreach (string card in deck)
+        {
+            print(card);
+        }
+        Deal();
+    }
+    private void Start()
+    {
+        StartGame();
+    }
+    public static List<string> GenerateDeck()
+    {
+        List<string> newDeck = new List<string>();
+        foreach (string t in type)
+        {
+            foreach (string v in values)
+            {
+                newDeck.Add(t + v);
+            }
+        }
+        return newDeck;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Kadangi nezinojau kaip shufflint kortas radau Fisher-Yates algoritma, kaip tai padaryti. 
+    void Shuffle<T>(List<T> list)
     {
-        
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            int k = random.Next(n);
+            n--;
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
+    }
+
+    void Deal()
+    {
+        // offsetas tam, kad patikrint ar visos kortos neatsirastu vienoje vietoje
+        float yOffset = 0;
+        foreach (string card in deck)
+        {
+            GameObject newCard = Instantiate(cardPrefab,new Vector3(transform.position.x,transform.position.y - yOffset, transform.position.z - zOffset), Quaternion.identity);
+            newCard.name = card;
+            newCard.GetComponent<CanSelect>().faceUp = true;
+
+            yOffset = yOffset + 0.4f;
+        }
     }
 }
--- Assets/Scripts/ChangeSprite.cs
+++ Assets/Scripts/ChangeSprite.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ChangeSprite : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private SpriteRenderer spriteRenderer;
+    private CanSelect canSelect;
+    private AI ai;
+
+    private void Start()
     {
-        
+        List<string> deck = AI.GenerateDeck();
+        ai = FindObjectOfType<AI>();
+
+        int i = 0;
+        foreach (string card in deck)
+        {
+            if (this.name == card)
+            {
+                cardFace = ai.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        canSelect = GetComponent<CanSelect>();
+
+    }
+    private void Update()
+    {
+        if (canSelect.faceUp == true)
+        {
+            spriteRenderer.sprite = cardFace;
+            transform.localScale = new Vector3(0.36f, 0.3892f, 0.3698f);
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+            transform.localScale = new Vector3(1.08f, 1.1678f, 1.1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
