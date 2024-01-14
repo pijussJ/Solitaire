abe2112d5cb10898131c561e8a2e7d6f2bcdf2da
pijussJ <pijus.jarusevicius3@gmail.com>
2024-01-14 15:46:16 +0200

    Neworking

--- Assets/CanSelect.cs
+++ Assets/CanSelect.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class CanSelect : MonoBehaviour
 {
     public bool faceUp = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class AI : MonoBehaviour
 {
     public Sprite[] cardFaces;
     public GameObject cardPrefab;
+    public GameObject[] bottomPos;
+    public GameObject[] topPos;
+
     public static string[] type = new string[] { "C", "H", "D", "S" };
     public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    public List<string>[] bottoms;
+    public List<string>[] tops;
+
+    private List<string> bottom0 = new List<string>();
+    private List<string> bottom1 = new List<string>();
+    private List<string> bottom2 = new List<string>();
+    private List<string> bottom3 = new List<string>();
+    private List<string> bottom4 = new List<string>();
+    private List<string> bottom5 = new List<string>();
+    private List<string> bottom6 = new List<string>();
+
 
     public List<string> deck;
     public void StartGame()
@@ -20,10 +35,12 @@ public class AI : MonoBehaviour
         {
             print(card);
         }
+        SolitaireSort();
         Deal();
     }
     private void Start()
     {
+        List<string> bottoms = new List<string> { "bottom0", "bottom1", "bottom2", "bottom3", "bottom4", "bottom5", "bottom6" };
         StartGame();
     }
     public static List<string> GenerateDeck()
@@ -56,15 +73,34 @@ public class AI : MonoBehaviour
 
     void Deal()
     {
+        for (int i = 0; i < 7; i++)
+        { 
+
+
         // offsetas tam, kad patikrint ar visos kortos neatsirastu vienoje vietoje
         float yOffset = 0;
-        foreach (string card in deck)
+            foreach (string card in bottoms[i])
+            {
+                GameObject newCard = Instantiate(cardPrefab, new Vector3(bottomPos[i].transform.position.x, bottomPos[i].transform.position.y - yOffset, bottomPos[i].transform.position.z), Quaternion.identity, bottomPos[i].transform);
+                newCard.name = card;
+                if (card == bottoms[i][bottoms[i].Count-1])
+                {
+                newCard.GetComponent<CanSelect>().faceUp = true;
+                }
+                yOffset = yOffset + 0.4f;
+            }
+        }
+    }
+    // funkcija kad isdalinti kortas
+    void SolitaireSort()
+    {
+        for (int i = 0; i < 7; i++)
         {
-            GameObject newCard = Instantiate(cardPrefab,new Vector3(transform.position.x,transform.position.y - yOffset, transform.position.z), Quaternion.identity);
-            newCard.name = card;
-            newCard.GetComponent<CanSelect>().faceUp = true;
-
-            yOffset = yOffset + 0.4f;
+            for (int j = i; j < 7; j++)
+            {
+                bottoms[j].Add(deck.Last<string>());
+                deck.RemoveAt(deck.Count - 1);
+            }
         }
     }
 }
